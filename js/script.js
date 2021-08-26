4620e460abcfae4cad395a9feed6f2cf34a97cd2
AndersonLeandrog <andersoncsql@hotmail.com>
2021-08-26 18:39:16 -0300

    1.0.0

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 
 function load() {
     var arrow = document.querySelector(".arrow");
-        arrow.addEventListener("click", expand);
-        arrow.addEventListener("dblclick", recolher);
+        arrow.addEventListener("click", expandirPagina);
+        arrow.addEventListener("dblclick", recolherPagina);
 
     var btnMore = document.querySelectorAll("button");
         btnMore[3].setAttribute("style", "width: 50px; height: 50px; border-radius: 50%; color: white; background: crimson;");
 }
 
-function expand() {
+function expandirPagina() {
     document.querySelector(".expand-Box").style.display="flex";
     document.querySelector(".arrow").style.animation="rotateSet 0.35s ease 1 both";
 }
 
-function recolher() {
+function recolherPagina() {
     document.querySelector(".expand-Box").style.display="none";
     document.querySelector(".arrow").style.animation="rotatenSet 0.35s ease 1 both";
 
